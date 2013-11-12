b942fea9faf2bef6858a783b65de72a8de935c4c
Jonathan <begaster@gmail.com>
2013-11-12 15:32:27 -0500

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  
+  #speed up Bcrypt-Ruby for test suite only
+  ActiveModel::SecurePassword.min_cost = true
+
 end
